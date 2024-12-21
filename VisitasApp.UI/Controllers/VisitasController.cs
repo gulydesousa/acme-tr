1101cde5bae26bd1c84017e0531dd473fea51532
Guly Mary de Sousa Garrido <gdesousa@gmail.com>
2024-12-21 19:45:17 +0100

    VisitasUpdateAsync

--- VisitasApp.UI/Controllers/VisitasController.cs
+++ VisitasApp.UI/Controllers/VisitasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using VisitasApp.Core.Domain.Entities;
 using VisitasApp.Core.DTO;
 using VisitasApp.Core.ServicesContract;
@@ -52,6 +53,39 @@ namespace VisitasApp.UI.Controllers
         }
 
 
+        [Route("[action]/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var visita = await _visitasService.VisitasGetByIdAsync(id);
+            if(visita == null)
+            {
+                return RedirectToAction("Index", "Visitas");
+            }
+            return View(visita);
+        }
 
+
+        [Route("[action]/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> Edit(Visita visitaUpdate)
+        {
+            Visita request = await _visitasService.VisitasGetByIdAsync(visitaUpdate.Id);
+
+            if (request == null)
+            {
+                return RedirectToAction("Index", "Visitas");
+            }
+            if (ModelState.IsValid)
+            {
+                await _visitasService.VisitasUpdateAsync(visitaUpdate);
+                return RedirectToAction("Index", "Visitas");
+            }
+            else
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                return View(request);
+            }
+        }
     }
 }
