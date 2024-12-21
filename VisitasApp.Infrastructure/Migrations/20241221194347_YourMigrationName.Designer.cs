e141ec31c8d3675314d4fad1e7df9c76981cb38b
Guly Mary de Sousa Garrido <gdesousa@gmail.com>
2024-12-21 20:49:23 +0100

    Migracion de EF

--- VisitasApp.Infrastructure/Migrations/20241221194347_YourMigrationName.Designer.cs
+++ VisitasApp.Infrastructure/Migrations/20241221194347_YourMigrationName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VisitasApp.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using VisitasApp.Infrastructure.Data;
 namespace VisitasApp.Infrastructure.Migrations
 {
     [DbContext(typeof(VisitasDbContext))]
-    partial class VisitasDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241221194347_YourMigrationName")]
+    partial class YourMigrationName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VisitasApp.Infrastructure/Migrations/20241221194347_YourMigrationName.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VisitasApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class YourMigrationName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Notas",
+                table: "Visitas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Notas",
+                table: "Visitas",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
